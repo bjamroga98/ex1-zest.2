b335a2502b54c1ea787feb259929b58c9175931c
barbara.jamroga <barbara.jamroga@microsoft.wsei.edu.pl>
2019-04-04 08:27:57 +0200

    Commit

--- ex1 zest.2/Program.cs
+++ ex1 zest.2/Program.cs
@@ -10,6 +10,25 @@ namespace ex1_zest._2
     {
         static void Main(string[] args)
         {
-        }
+            int[,] array = new array { { 1, 5, 8 }, { 7, 9, 4 }, { 2, 1, 0 }, { 6, 0, 3 } };
+            for (int i = 0; i<array.GetLength(0); ++i)
+            {
+                for (int j = 0; j<array.GetLength(0); ++j)
+                {
+                    Console.Write("{0} ", array[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            for (int i = 0; i<array.GetLength(0); ++i)
+            {
+                int rowsum = 0;
+                for (int j = 0; j<array.GetLength(0); ++j)
+                {
+                    rowsum += array[i, j];
+                }
+                Console.WriteLine("Sum of row {0}:   {1}", i, rowsum);
+            }
+        }Console.Readkey();
     }
 }
